92a76aa3a634501964738aaee06ec5ccdf95d66d
peña duarte <67807952+dannypen@users.noreply.github.com>
2020-07-08 11:58:22 -0500

    resta

---
+++ pruebamds.py
@@ -0,0 +1,9 @@
+print(¨operaciones¨)
+a=float(input(¨ingrese el primer numero:¨))
+b=float(input(¨ingrese el segundo termino¨))
+sum=a+b
+print(¨****suma****¨)
+print(a,¨+¨,b,¨=¨,sum)
+print(¨***resta***¨)
+rest=a-b
+print(a,¨-¨,b,¨=¨,rest)
